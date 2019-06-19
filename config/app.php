dd5ddcef6ea086e839d5c093d01acaed43416827
Kasun Vimarshana <KasunV@brandix.com>
2019-06-19 11:12:36 +0530

    install js plugins

--- config/app.php
+++ config/app.php
@@ -67,7 +67,9 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    //'timezone' => 'UTC',
+    //'timezone' => 'Asia/Colombo',
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
@@ -174,6 +176,14 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // custom service providers
+        App\Providers\AuthUserProvider::class,
+        // provider for flash message
+        Codecourse\Notify\NotifyServiceProvider::class,
+        // provider for bensampo/laravel-enum (Laravel < 5.5)
+        // BenSampo\Enum\EnumServiceProvider
+        // provider for chumper\zipper
+        Chumper\Zipper\ZipperServiceProvider::class
 
     ],
 
@@ -225,6 +235,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //alias for flash message
+        'Notify' => Codecourse\Notify\Facades\Notify::class,
+        //alias for chumper\zipper
+        'Zipper' => Chumper\Zipper\Zipper::class
 
     ],
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,97 +3,57 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>Welcome</title>
+        
+        <!-- Bootstrap --> 
+        <link rel="stylesheet" href="{{ asset('node_modules/admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css') }}"/>
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="{{ asset('node_modules/admin-lte/bower_components/font-awesome/css/font-awesome.min.css') }}"/>
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="{{ asset('node_modules/admin-lte/bower_components/Ionicons/css/ionicons.min.css') }}"/>
+        <!-- Theme style -->
+        <link rel="stylesheet" href="{{ asset('node_modules/admin-lte/dist/css/AdminLTE.min.css') }}"/>
+        <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+        page. However, you can choose any other skin. Make sure you
+        apply the skin class to the body tag so the changes take effect. -->
+        <link rel="stylesheet" href="{{ asset('node_modules/admin-lte/dist/css/skins/_all-skins.min.css') }}"/>
+        <!-- Jquery Easy Loading -->
+        <link rel="stylesheet" href="{{ asset('node_modules/jquery-easy-loading/dist/jquery.loading.min.css') }}"/>
+        <!-- custom stylesheet -->
+        <link rel="stylesheet" href="{{ asset('css/custom_style.css') }}"/>
+        <link rel="stylesheet" href="{{ asset('css/custom_scrollbar.css') }}"/>
+        
+        <!-- jQuery 3 -->
+        <script src="{{ asset('node_modules/admin-lte/bower_components/jquery/dist/jquery.min.js') }}"></script>
+        <!-- Bootstrap 3.3.7 -->
+        <script src="{{ asset('node_modules/admin-lte/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+        <!-- AdminLTE App -->
+        <script src="{{ asset('node_modules/admin-lte/dist/js/adminlte.min.js') }}"></script>
+        <!-- pooper js -->
+        <script src="{{ asset('node_modules/popper.js/dist/umd/popper.min.js') }}"></script>
+        <!-- bootbox -->
+        <script src="{{ asset('node_modules/bootbox/dist/bootbox.min.js') }}"></script>
+        <script src="{{ asset('node_modules/bootbox/dist/bootbox.locales.min.js') }}"></script>
+        <!-- Moment -->
+        <script src="{{ asset('node_modules/admin-lte/bower_components/moment/min/moment.min.js') }}"></script>
+        <!-- InputMask -->
+        <script src="{{ asset('node_modules/admin-lte/plugins/input-mask/jquery.inputmask.js') }}"></script>
+        <script src="{{ asset('node_modules/admin-lte/plugins/input-mask/jquery.inputmask.extensions.js') }}"></script>
+        <!-- Jquery Easy Loading -->
+        <script src="{{ asset('node_modules/jquery-easy-loading/dist/jquery.loading.min.js') }}"></script>
+        <!-- Bootstrap Validation -->
+        <script src="{{ asset('node_modules/bootstrap-validator/dist/validator.min.js') }}"></script>
+        
+        <!-- *************************************************** -->
+        <script src="{{ asset('node_modules/conva-length/dist/length.js') }}" charset="utf-8"></script>
+        <script src="{{ asset('node_modules/konva/konva.min.js') }}" charset="utf-8"></script>
+        <script src="{{ asset('node_modules/fabric/dist/fabric.js') }}" charset="utf-8"></script>
+        <!-- *************************************************** -->
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            
         </div>
     </body>
 </html>
+conva
